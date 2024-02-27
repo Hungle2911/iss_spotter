fbdf89fc844c9b185c3559292c1a7e5ae07baeeb
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-02-27 14:00:33 -0500

    add fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -6,20 +6,40 @@
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-const request = require('request')
-const url = 'https://api.ipify.org?format=json'
-const fetchMyIP = function(callback) { 
+const request = require('request');
+const url = 'https://api.ipify.org?format=json';
+const fetchMyIP = function(callback) {
   request(url, (error, response, data) => {
     if (error) return callback(error, null);
 
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${data}`), null);
       return;
     }
 
     const ip = JSON.parse(data).ip;
     callback(null, ip);
-  })
-}
+  });
+};
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
 
-module.exports = { fetchMyIP };
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, {latitude, longitude});
+  });
+};
+module.exports = { fetchMyIP, fetchCoordsByIP };
